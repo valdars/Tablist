15d50b5e8abfdfeaab5ba7e1d053df7c2d00f37f
Valdar Vaht <valdars@gmail.com>
2018-01-16 00:27:39 +0200

    Update manifest Add locking

--- tabmanager.js
+++ tabmanager.js
@@ -1,7 +1,10 @@
+var lockList = [];
+
 // get all tabs and create html for tab list
 function updateTabList() {
     var content = document.createDocumentFragment();
     browser.tabs.query({}).then(tabs => {
+        //console.log(tabs[0]);
         for (let tab of tabs) {
             let tabId = tab.id;
             let row = $("<tr>");
@@ -10,21 +13,36 @@ function updateTabList() {
             favicon.append($('<img class="favicon">').prop('src', tab.favIconUrl));
             row.append(favicon);
 
-            let titleLink = $('<a href="javascript:void();">'+tab.title+'</a>');
+            let titleLink = $(`<a href="javascript:void();">${tab.id} ${tab.title}</a>`);
             titleLink.on('click', () => {
-                browser.tabs.update(tabId, {active: true});
+                browser.tabs.update(tabId, { active: true });
             });
             let title = $('<td>').append(titleLink);
             row.append(title);
 
-            let close = $('<td>');
+            let commands = $('<td>');
+
+            let lockBtn = '';
+            if(isLocked(tab.url)) {
+                lockBtn = $('<a class="button"><span class="icon is-large has-text-success"><i class="fa fa-lg fa-unlock"></i></span></a>');
+                lockBtn.on('click', () => {
+                    unlock(tab.url);
+                });
+            }else {
+                lockBtn = $('<a class="button"><span class="icon is-large has-text-danger"><i class="fa fa-lg fa-lock"></i></span></a>');
+                lockBtn.on('click', () => {
+                    lock(tab.url);
+                });
+            }
+            commands.append(lockBtn);
+
             let closeBtn = $('<a class="button"><span class="icon is-large"><i class="fa fa-lg fa-window-close-o"></i></span></a>');
-            
             closeBtn.on('click', () => {
                 browser.tabs.remove(tabId);
             });
-            close.append(closeBtn);
-            row.append(close);
+            commands.append(closeBtn);
+
+            row.append(commands);
 
             content.appendChild(row[0]);
         }
@@ -65,6 +83,36 @@ function updateClosedTabList() {
     });
 }
 
+function updateLockList() {
+    browser.storage.sync.get({locked: {}})
+    .then(results => {
+        lockList = results.locked;
+        updateTabList();
+    });
+}
+
+function saveLockList() {
+    browser.storage.sync.set({
+        locked: lockList
+    }).then(() => {
+        updateTabList();
+    });
+}
+
+function isLocked(url) {
+    return lockList.includes(url);
+}
+
+function lock(url) {
+    lockList.push(url);
+    saveLockList();
+}
+
+function unlock(url) {
+    lockList = lockList.filter(x => x != url);
+    saveLockList();
+}
+
 // init html tabs functionality
 function initTabs() {
     $('[data-tablink]').on('click', (event) => {
@@ -95,5 +143,6 @@ browser.tabs.onCreated.addListener(updateClosedTabList);
 
 //  initialization code
 updateTabList();
+updateLockList();
 updateClosedTabList();
 initTabs();
